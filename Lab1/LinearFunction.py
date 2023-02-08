73e5d606ae43083835ec1c652939ad9ef90ef32a
Snow Lukin <everyoursden@gmail.com>
2023-02-08 11:26:03 +0300

    print coeffs

--- Lab1/LinearFunction.py
+++ Lab1/LinearFunction.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 class Lab1:
     def __init__(self):
         # Table values
+        # self.x = np.array([1, 2, 3, 4, 5, 6])
+        # self.y = np.array([1, 1.5, 3, 4.5, 7, 8.5])
         self.x = np.array([3, 5, 7, 9, 11, 13])
         self.y = np.array([3.5, 4.4, 5.7, 6.1, 6.5, 7.3])
         # self.x = np.array([10, 20, 30, 40, 50, 60])
         # self.y = np.array([1.06, 1.33, 1.52, 1.68, 1.81, 1.91])
         # Mean values
-        self.x_mean = np.mean(self.x)
-        self.y_mean = np.mean(self.y)
+        # self.x_mean = np.mean(self.x)
+        # self.y_mean = np.mean(self.y)
         pass
 
     def linear_function(self, show_plot=True):
@@ -20,10 +22,13 @@ class Lab1:
         matrix = np.vstack([self.x, np.ones(len(self.x))]).T
         # Calculate a and b
         a, b = np.linalg.lstsq(matrix, self.y, rcond=None)[0]
+        print('a: {}, b: {}', a, b)
         # Predict y
         y_pred = a * self.x + b
         # Plot the data and the approximation
         self.plot_result(y_pred, color='red', show_plot=show_plot)
+        # Print comparison
+        self.task_compare(y_pred, name='Linear')
         return y_pred
 
     def power_function(self, show_plot=True):
@@ -35,10 +40,14 @@ class Lab1:
         matrix = np.vstack([x_log, np.ones(len(x_log))]).T
         # Calculate a and b
         a, b = np.linalg.lstsq(matrix, y_log, rcond=None)[0]
+        # print(a, b)
+        print('a: {}, b: {}', a, np.exp(b))
         # Predict y
         y_pred = np.exp(a * np.log(self.x) + b)
         # Plot the data and the approximation
         self.plot_result(y_pred, color='green', show_plot=show_plot)
+        # Print comparison
+        self.task_compare(y_pred, name='Power')
         return y_pred
 
     def exponential_function(self, show_plot=True):
@@ -47,10 +56,13 @@ class Lab1:
         matrix = np.vstack([self.x, np.ones(len(self.x))]).T
         # Calculate a and b
         a, b = np.linalg.lstsq(matrix, np.log(self.y), rcond=None)[0]
+        print('a: {}, b: {}', a, np.exp(b))
         # Predict y
         y_pred = np.exp(a * self.x + b)
         # Plot the data and the approximation
         self.plot_result(y_pred, color='orange', show_plot=show_plot)
+        # Print comparison
+        self.task_compare(y_pred, name='Exponential')
         return y_pred
 
     def quadratic_function(self, show_plot=True):
@@ -59,10 +71,14 @@ class Lab1:
         matrix = np.vstack([self.x ** 2, self.x, np.ones(len(self.x))]).T
         # Calculate a, b and c
         a, b, c = np.linalg.lstsq(matrix, self.y, rcond=None)[0]
+        print('a: {}, b: {}, c: {}', a, b, c)
+        print(a, b, c)
         # Predict y
         y_pred = a * self.x ** 2 + b * self.x + c
         # Plot the data and the approximation
         self.plot_result(y_pred, color='purple', show_plot=show_plot)
+        # Print comparison
+        self.task_compare(y_pred, name='Quadratic')
         return y_pred
 
     def plot_result(self, y_pred, color='red', show_plot=True):
@@ -117,3 +133,9 @@ class Lab1:
     def compare_mse_mae(self):
         self.compare_mse()
         self.compare_mae()
+
+    def task_compare(self, y_pred, name):
+        result = 0
+        for i in range(len(self.y)):
+            result += (self.y[i] - y_pred[i]) ** 2
+        print('{} = {}', name, result)
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from Lab1.LinearFunction import Lab1
 
 def start_lab1():
     lab1 = Lab1()
-    lab1.compare_mse()
-    lab1.compare_mae()
+    # lab1.compare_mse()
+    # lab1.compare_mae()
     lab1.plot_all_results()
 
+
+start_lab1()
